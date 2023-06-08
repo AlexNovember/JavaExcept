5e2bba2fc11fab66cc943695b15b3d69ef4f71bb
Alex <tanko77@rambler.ru>
2023-06-08 10:55:56 +0300

    2

--- lesson2.java
+++ lesson2.java
@@ -3,29 +3,38 @@ import java.util.Scanner;
 public class lesson2 {
     public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            boolean valid;
-            Float myNumber = null;
-            String inputData;
-            do {
-                System.out.print("Введите число:\t");
-                try {
-                    inputData = scanner.nextLine();
-                    if (Float.parseFloat(inputData) > 0) {
-                        myNumber = Float.parseFloat(inputData);
-                        valid = true;
-                    } else {
-                        valid = false;
-                    }
-                } catch (Exception ex) {
-                    System.out.println("Ввели не число, попробуйте еще раз");
-                    valid = false;
-                }
-            } while (!valid);
-            System.out.println("Вы ввели: " + myNumber);
-        }
+    //     try (Scanner scanner = new Scanner(System.in)) {
+    //         boolean valid;
+    //         Float myNumber = null;
+    //         String inputData;
+    //         do {
+    //             System.out.print("Введите число:\t");
+    //             try {
+    //                 inputData = scanner.nextLine();
+    //                 if (Float.parseFloat(inputData) > 0) {
+    //                     myNumber = Float.parseFloat(inputData);
+    //                     valid = true;
+    //                 } else {
+    //                     valid = false;
+    //                 }
+    //             } catch (Exception ex) {
+    //                 System.out.println("Ввели не число, попробуйте еще раз");
+    //                 valid = false;
+    //             }
+    //         } while (!valid);
+    //         System.out.println("Вы ввели: " + myNumber);
+    //     }
+    // }
+    
+    try {
+        int d = 0;
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        double catchedRes1 = intArray[8] / d;
+        System.out.println("catchedRes1 = " + catchedRes1);
+    } catch (ArithmeticException e) {
+        System.out.println("Catching exception: " + e);
+    }
     }
 }
-
 
 
